b7e8c1b4655b1f01eeeb65cefaafa6133b73883e
Brandon Chavez <brandon.nan.chavez@gmail.com>
2022-02-06 13:42:28 -0800

    Complete basic structure of CostTable row components.

--- feesaurus-js/src/components/CostTable.js
+++ feesaurus-js/src/components/CostTable.js
@@ -4,22 +4,26 @@ export default class CostTable extends React.Component {
     render() {
         return (
             <div className = 'cost-table'>
+                <br/>
                 <HeaderRow/>
-                <AttrCostRow/>
-                <AttrCostRow/>
-                <AttrCostRow/>
-                <AttrCostRow/>
-                <AggrCostRow/>
+                <br/>
+                <AttrCostRow name='Exam?' type='radio'/>
+                <AttrCostRow name='Lens Type' type='select'/>
+                <AttrCostRow name='Material' type='select'/>
+                <AttrCostRow name='Coating' type='select'/>
+                <AttrCostRow name='Tint' type='select'/>
+                <br/>
+                <AggrCostRow name='Misc. Treatments'/>
                 {/* Misc. Attributes Section */}
-                <AttrCostRow/>
-                <AttrCostRow/>
-                <AttrCostRow/>
-                <AttrCostRow/> 
-                <AttrCostRow/> 
-                <AttrCostRow/> 
-                <AttrCostRow/> 
-                <AttrCostRow/> 
-                <AttrCostRow/>  
+                <AttrCostRow name='Polish, coat, paint, groove, rimless mount' type='checkbox'/>
+                <AttrCostRow name='Drilling: 2 or 4 holes' type='checkbox'/>
+                <AttrCostRow name='Oversize (>= 56mm)' type='checkbox'/>
+                <AttrCostRow name='Slab Off Prism' type='checkbox'/> 
+                <AttrCostRow name='Beveled Edges, Facets - One Side' type='checkbox'/> 
+                <AttrCostRow name='Beveled Edges, Facets - Two Sides' type='checkbox'/> 
+                <AttrCostRow name='Wrap Frame' type='checkbox'/> 
+                <AttrCostRow name='UV Coating' type='checkbox'/> 
+                <AttrCostRow name='Mirror Coating' type='checkbox'/>  
                 <TotalRow/>        
             </div>
         );
@@ -66,7 +70,7 @@ function MenuSelector(props) {
 
 function RadioButtonSelector(props) {
     return(
-        <div>
+        <div style={inline}>
             <input type='radio' id='yes' name={props.name}/> Yes
             <input type='radio' id='no' name={props.name}/> No
         </div>
@@ -75,33 +79,41 @@ function RadioButtonSelector(props) {
 
 function CheckboxSelector(props) {
     return(
-        <div>
+        <div style={inline}>
             <input type='checkbox' id={props.name} name={props.name}/>
-            <label for={props.name}></label>
         </div>
     );
 }
 
-class AttrCostRow extends React.Component {
-    render() {
-        return (
-            <h2>Attribute Cost Row</h2>
-        );
-    }
+/* Temporary styling to prevent headers from breaking onto their own line (better readability during testing). */
+const inline = {
+    display: 'inline'
+};
+
+
+function AttrCostRow(props) {
+    return (
+        <div>
+            <label htmlFor={props.name}>{props.name}</label>
+            <AttrSelector name={props.name} type={props.type}/>
+            <h4 style={inline}>$10</h4>
+        </div>
+    );
 }
 
-class AggrCostRow extends React.Component {
-    render() {
-        return (
-            <h2>Aggregate Cost Row</h2>
-        );
-    }
+function AggrCostRow(props) {
+    return (
+        <div>
+            <label htmlFor={props.name}>{props.name}</label>
+            <h4 style={inline}>$90</h4>
+        </div>
+    );
 }
 
 function TotalRow(props){
     return (
         <div>
-            <h3>Total Pt. Responsibility: {props.totalCost}</h3>
+            <h3>Total Pt. Responsibility: 0</h3>
         </div>
     );
 }
